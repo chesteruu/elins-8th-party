576467d21cd6c7cc92b054529f554972d063f1bf
gang.xu <gang.xu@klarna.com>
2025-03-09 16:02:38 +0100

    Use the string formatted id

--- netlify/functions/updateGuest.js
+++ netlify/functions/updateGuest.js
@@ -30,7 +30,7 @@ exports.handler = async (event) => {
     
     // Build the FQL query dynamically
     const result = await client.query(fql`
-      guests.byId(${id}).update({
+      guests.byId("${id}").update({
           message: ${data.message},
           confirmed: ${data.confirmed},
           attending: ${data.attending}
